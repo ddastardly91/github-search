1d2c2ce530cda995aed1144c267ce412b63b7396
Aaron Latham <alatham7891@gmail.com>
2022-11-15 21:38:57 +0000

    ADDED: User page

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -9,6 +9,8 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 export const GithubProvider = ({ children }) => {
    const initialState = {
       users: [],
+      user: {},
+      repos: [],
       loading: false,
    };
 
@@ -28,11 +30,65 @@ export const GithubProvider = ({ children }) => {
          },
       });
 
-      const { items } = await response.json();
-      dispatch({
-         type: "GET_USERS",
-         payload: items,
+      if (response.status === 404) {
+         window.location = "/notfound";
+      } else {
+         const { items } = await response.json();
+         dispatch({
+            type: "GET_USERS",
+            payload: items,
+         });
+      }
+   };
+
+   // Get a single user
+   const getUser = async (login) => {
+      setLoading();
+
+      const response = await fetch(`${GITHUB_URL}/users/${login}`, {
+         headers: {
+            Authorization: `token ${GITHUB_TOKEN}`,
+         },
       });
+
+      if (response.status === 404) {
+         window.location = "/notfound";
+      } else {
+         const data = await response.json();
+         dispatch({
+            type: "GET_USER",
+            payload: data,
+         });
+      }
+   };
+
+   // Get user repos
+   const getUserRepos = async (login) => {
+      setLoading();
+
+      const params = new URLSearchParams({
+         sort: "created",
+         per_page: 10,
+      });
+
+      const response = await fetch(
+         `${GITHUB_URL}/users/${login}/repos?${params}`,
+         {
+            headers: {
+               Authorization: `token ${GITHUB_TOKEN}`,
+            },
+         }
+      );
+
+      if (response.status === 404) {
+         window.location = "/notfound";
+      } else {
+         const data = await response.json();
+         dispatch({
+            type: "GET_REPOS",
+            payload: data,
+         });
+      }
    };
 
    const setLoading = () => {
@@ -47,9 +103,13 @@ export const GithubProvider = ({ children }) => {
       <GithubContext.Provider
          value={{
             users: state.users,
+            user: state.user,
             loading: state.loading,
+            repos: state.repos,
             searchUsers,
             clearUsers,
+            getUser,
+            getUserRepos,
          }}
       >
          {children}
